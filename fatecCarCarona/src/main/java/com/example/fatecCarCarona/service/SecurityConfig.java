95056fa0448c8d765916b444c5d6493c5778b14f
unknown <rufinoguilherme633@gmail.com>
2026-05-21 21:41:07 -0300

    security

--- fatecCarCarona/src/main/java/com/example/fatecCarCarona/service/SecurityConfig.java
+++ fatecCarCarona/src/main/java/com/example/fatecCarCarona/service/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001","https://fatec-ride-frontend2-0-hnup7hh8p-caronafatec-hashs-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // permite cookies e headers como Authorization
